99658e9c5926ec782acec81748ed957d17c9b5a2
Ismail Saleem <113327836+ismail-ilm66@users.noreply.github.com>
2026-04-15 17:13:23 +0500

    Merge pull request #2 from ismail-ilm66/ui/fonts-implementation
    feat: update subscriptions component to use styled SafeAreaView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,23 @@
 import "@/global.css";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <SafeAreaView>
-        <Text className="text-xl font-bold text-success ">
-          Welcome to Nativewind!
-        </Text>
-        <Link href="/sign-up" className="text-success h-7">
-          Sign Up
-        </Link>
+    <SafeAreaView className="flex-1   bg-background p-5">
+      <Text className="text-5xl font-sans-bold  ">Home!</Text>
+      <Link href="/sign-up" className="text-success h-7">
+        Sign Up
+      </Link>
 
-        <Link href="/subscriptions/claude" className="text-success">
-          Go To Subscription
-        </Link>
-      </SafeAreaView>
-    </View>
+      <Link href="/subscriptions/claude" className="text-success">
+        Go To Subscription
+      </Link>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,6 +1,9 @@
+import { styled } from "nativewind";
 import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 const Subscriptions = () => {
   return (
     <SafeAreaView>
